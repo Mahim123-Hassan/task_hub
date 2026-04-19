7b9a5fff7959dff14c0df7ab7bba8dc99544eebb
Mahim123-Hassan <hmahim951@gmail.com>
2026-04-19 23:01:03 +0600

    added post_request_api

--- lib/data/network_caller.dart
+++ lib/data/network_caller.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart';
 
 class NetworkCaller {
-  Future<NetworkResponse> getRequest(String Url) async {
+  Future<NetworkResponse> getRequest(String url) async {
     try {
-      Uri uri = Uri.parse(Url);
+      Uri uri = Uri.parse(url);
+      _logRequest(url);
       Response response = await get(uri);
+      _logResponse(url, response);
       final decodedData = jsonDecode(response.body);
       if (response.statusCode == 200) {
         return NetworkResponse(
@@ -28,6 +31,56 @@ class NetworkCaller {
       );
     }
   }
+
+  Future<NetworkResponse> postRequest(
+    String url,
+    Map<String, dynamic>? body,
+  ) async {
+    try{
+    Uri uri = Uri.parse(url);
+    _logRequest(url,body: body);
+
+    Response response = await post(
+      uri,
+      headers: {'Content-type': 'application/json'},
+      body: jsonEncode(body),
+    );
+    _logResponse(url, response);
+
+    final decodedData = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return NetworkResponse(
+        isSuccess: true,
+        responseCode: response.statusCode,
+        body: decodedData,
+      );
+    } else {
+      return NetworkResponse(
+        isSuccess: false,
+        responseCode: response.statusCode,
+      );
+    }
+  }catch(e){
+      return NetworkResponse(isSuccess: false,
+          responseCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+}
+
+void _logRequest(String url, {Map<String, dynamic>? body}) {
+  debugPrint("URL:$url\n"
+      "Body:$body");
+}
+
+void _logResponse(String url, Response response) {
+  debugPrint(
+    "URL:$url\n"
+    "Status Code:${response.statusCode}\n"
+    "Body:${response.body}",
+  );
 }
 
 class NetworkResponse {
